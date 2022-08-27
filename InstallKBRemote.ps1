2b3256d7ab2157a921bca8d345fcffe503aded1f
Dan <31004901+DanGOTO100@users.noreply.github.com>
2022-08-28 01:20:59 +0200

    Update InstallKBRemote.ps1

--- InstallKBRemote.ps1
+++ InstallKBRemote.ps1
@@ -1,5 +1,5 @@
 <#
-MVP of SCript - Note it uses PSWindowsupdate module - for remotely updating a KBXXXXX missing in a list of identified servers list (Server1, Server2, etc..) where is it missing. 
+MVP of SCript - Note it uses PSWindowsupdate module - for remotely updating a KBXXXXX missing in a list of identified servers list (Server1, Server2, etc..) where is it missing. Output of the job goes to JobOutput.log file.
 #>
 
 Invoke-WUJob -ComputerName "list of Server1, Server2, Server3"  -Script {ipmo PSWindowsUpdate; Install-WindowsUpdate -KBArticleID KBXXXXX  -AcceptAll | Out-File "JobOutput.log" } -RunNow -Confirm:$false -Verbose -ErrorAction Ignore
